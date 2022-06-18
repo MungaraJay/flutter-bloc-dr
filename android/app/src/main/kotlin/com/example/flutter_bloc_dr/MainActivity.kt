9283f9ea61a26b36072486d02bcc743086ebe192
jaymungara-solutelabs <jay.m@solutelabs.com>
2022-06-18 10:05:35 +0530

    Initial Project Setup

---
+++ android/app/src/main/kotlin/com/example/flutter_bloc_dr/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_bloc_dr
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/utils/app_routes.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc_dr/feature/presentation/screen/home/presentation/home_screen.dart';
+import 'package:flutter_bloc_dr/feature/presentation/screen/splash_screen.dart';
+import 'package:flutter_bloc_dr/utils/app_constant.dart';
+
+final Map<String, WidgetBuilder> appRoutes = {
+  AppConstant.routeSplashScreen: (context) => const SplashScreen(),
+  AppConstant.routeHomeScreen: (context) => const HomeScreen(),
+};
